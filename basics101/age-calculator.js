c7b81ea9f422b5b02a9289eae67746d1ee1523c7
danny1480 <44119028+danny1480@users.noreply.github.com>
2018-10-18 18:56:52 -0400

    Add files via upload

---
+++ basics101/age-calculator.js
@@ -0,0 +1,3 @@
+var yearofBirth = 1992;
+var yearofMarriage = 2020;
+console.log('I will be either: ' + (yearofMarriage-yearofBirth) + ' or: ' + (yearofMarriage-yearofBirth+1));
---
+++ basics101/bio-details.js
@@ -0,0 +1,6 @@
+var myPartnersName = 'Spouse - Deepika Padukone';
+var numberofChildren = 'No. of kids-10, 5 boys, 5 Girls';
+var location = 'lives in Hawaii';
+var myJob = 'Full Stack Engineer-works remotely';
+console.log(myPartnersName, numberofChildren, myJob, location);
+// Did all at once instead of one by one
