08f276d3e075d4aa3c43a6dabeb12055a86decaa
danny1480 <44119028+danny1480@users.noreply.github.com>
2018-10-25 20:07:47 -0400

    Add files via upload

---
+++ basics101/strings-01.js
@@ -0,0 +1,14 @@
+let firstname = 'Daiyan';
+let lastname = 'Khan';
+
+
+let fullname = firstname + ' ' + lastname;
+console.log(fullname);
+
+
+//homework
+//find out if the word daiyan is included in fullname
+//find out which character is at index 8 of fullname
+
+console.log(fullname.charAt(8));
+console.log(fullname.includes('Daiyan'));
