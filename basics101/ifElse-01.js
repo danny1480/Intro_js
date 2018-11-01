1963df2ffc24936d5ae544748a73124d7a6dad97
danny1480 <44119028+danny1480@users.noreply.github.com>
2018-11-01 18:16:15 -0400

    Add files via upload

---
+++ basics101/ifElse-01.js
@@ -0,0 +1,12 @@
+let isCustomerOneVegan = false;
+let isCustomerTwoVegan = false;
+
+if (isCustomerOneVegan && isCustomerTwoVegan){
+    console.log('Please Check our Vegetable Menu :');
+} else if (isCustomerOneVegan || isCustomerTwoVegan){
+    console.log('Please Check our Veg and Chicken Menu :');
+} else {
+    console.log('Please Check our Full Menu :')
+}
+    
+ 
---
+++ basics101/switch-01.js
@@ -0,0 +1,25 @@
+let month = 'December';
+
+switch(month) {
+   case 'December': 
+   case 'January':
+   case 'February':
+   case 'March':
+       console.log('Winter');
+       break;
+   case 'April':
+   case 'May':
+       console.log('Spring');
+       break;
+   case 'June':
+   case 'July':
+   case 'August':
+   case 'Spetmeber':
+       console.log('Summer');
+       break;
+   case 'October':
+   case 'November':
+       console.log('Fall');
+       break;
+        
+}
